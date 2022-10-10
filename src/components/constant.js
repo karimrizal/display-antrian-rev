1f3e7143b8e6e6d0aeaf63406a1c885e502842f7
wpsaputra <wayanps93@gmail.com>
2022-10-10 16:03:03 +0800

    add sound in static bundle todo customize message

--- src/components/constant.js
+++ src/components/constant.js
@@ -1,4 +1,7 @@
+import camera_sound from '../assets/sound/camera-shutter-click.mp3';
+
 export const url_api = "https://sultradata.com/project/antrian-api/api.php";
 export const url_api_view = "https://sultradata.com/project/antrian-api/api-view.php";
 export const url_upload = "https://sultradata.com/project/antrian-api/upload.php";
-export const url_camera_sound = "https://sultradata.com/project/antrian-api/camera-shutter-click.mp3";
+// export const url_camera_sound = "https://sultradata.com/project/antrian-api/camera-shutter-click.mp3";
+export const url_camera_sound = camera_sound;
