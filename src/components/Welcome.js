026b4e6e661d1eb171511375292b0535ece59107
wpsaputra <wayanps93@gmail.com>
2022-10-31 08:51:55 +0800

    change welcome to landscape

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -10,15 +10,17 @@ import "../custom.css"
 
 export default function Welcome() {
   return (
-    <section className=" bg-gradient-to-r from-gradient3/30 via-gradient2/80 to-gradient1/40 ...">
+    <section className=" bg-gradient-to-r from-gradient3/30 via-gradient2/80 to-gradient1/40 ..." style={{height: "100vh", display: "flex", flexDirection: "column", justifyContent: "space-around"}}>
       <div className="flex text-center mx-auto items-center justify-center lg:mt-0 sm:w-full md:w-3/4 lg:w-1/2 xl:2-1/2">
         <a href="https://sultradata.com/project/antrian-backend">
           <img src={logo}
             alt="logo" className="object-contain py-6" /></a>
       </div>
+      
+      <h2 className="text-3xl font-bold text-center tracking-tight text-pst" style={{ marginBottom: 10 }}>Selamat Datang di PST BPS Provinsi Sulawesi Tenggara 👋</h2>
+      
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:max-w-7xl lg:px-8">
 
-        <h2 className="text-3xl font-bold text-center tracking-tight text-pst" style={{ marginBottom: 10 }}>Selamat Datang di PST BPS Provinsi Sulawesi Tenggara 👋</h2>
         {/* <div className='py-3 mt-3 mb-12 columns-1 px-2 text-center'>
             <Link to="/daftar" className="items-center justify-center px-4 py-4 rounded-xl shadow-sm text-base font-medium text-white bg-pst hover:bg-blue hover:text-white">
               Ambil Antrian
@@ -27,8 +29,35 @@ export default function Welcome() {
               Survey Kepuasan
             </Link>
           </div> */}
+        
+        <div className="grid grid-cols-2 gap-4">
+          <div>
+            <Link to="/daftar" className="text-3xl items-center justify-center px-4 py-4 rounded-xl shadow-md text-base font-medium text-black bg-pst hover:bg-blue hover:text-white custombutton"
+              style={{ display: "flex", alignItems: "center", flexDirection: "column", padding: "30px", backgroundColor: "#ffc500", height: "100%" }}
+            >
+              <img className="w-auto"
+                style={{height: "100%"}}
+                src={line_up}
+                alt="logo"
+              />
+              Ambil Antrean
+            </Link>
+          </div>
+          <div>
+            <Link to="/survey" className="text-3xl items-center justify-center px-4 py-4 rounded-xl shadow-md text-base font-medium text-white bg-pst hover:bg-blue hover:text-white custombutton"
+                style={{ display: "flex", alignItems: "center", flexDirection: "column", padding: "40px", height: "100%" }}
+            >
+              <img className="w-auto"
+                src={smiley}
+                alt="logo"
+              />
+              Survey Kepuasan
+            </Link>
 
-        <div className="full-remaining-height">
+          </div>
+        </div>
+
+        {/* <div className="full-remaining-height">
           <div class="grid grid-col-1 gap-0 py-9">
             <div></div>
             <Link to="/daftar" className="text-3xl items-center justify-center px-4 py-4 rounded-xl shadow-md text-base font-medium text-black bg-pst hover:bg-blue hover:text-white custombutton"
@@ -65,7 +94,7 @@ export default function Welcome() {
           </div>
 
 
-        </div>
+        </div> */}
 
 
 
