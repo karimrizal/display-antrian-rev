a8cc70458de45f27d101a18b57f015e00cf50917
wpsaputra <wayanps93@gmail.com>
2022-12-14 15:05:16 +0800

    testbox version

--- src/components/constant.js
+++ src/components/constant.js
@@ -1,9 +1,9 @@
 import camera_sound from '../assets/sound/camera-shutter-click.mp3';
 
-export const url_api = "https://sultradata.com/project/antrian-api/api.php";
-export const url_api_view = "https://sultradata.com/project/antrian-api/api-view.php";
-export const url_upload = "https://sultradata.com/project/antrian-api/upload.php";
+export const url_api = "https://api.testbox.web.id/demo-antrian/api.php";
+export const url_api_view = "https://api.testbox.web.id/demo-antrian/api-view.php";
+export const url_upload = "https://api.testbox.web.id/demo-antrian/upload.php";
 // export const url_camera_sound = "https://sultradata.com/project/antrian-api/camera-shutter-click.mp3";
 export const url_camera_sound = camera_sound;
-export const url_image = "https://sultradata.com/project/antrian-api/";
+export const url_image = "https://api.testbox.web.id/demo-antrian/";
 export const url_image_onedata = "https://sultradata.com/project/onedataasn/storage/app/public/foto/";
